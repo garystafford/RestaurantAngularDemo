d817f83449a2b2fe728ef508cd5951a186a1586d
Gary Stafford <gary.stafford@accenture.com>
2017-10-27 22:54:03 -0400

    Working but not using Order Service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,9 @@ import {MenuItem} from './menu-item';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {Order} from './order';
 import {OrderItem} from './order-item';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
 import {IOrderResponse} from './order-response';
+import 'rxjs/add/operator/retry';
 
 
 @Component({
@@ -39,8 +40,20 @@ export class AppComponent implements OnInit {
     if (this.order.items.length > 0) {
       const headers = new HttpHeaders({'Content-Type': 'application/json'});
       this.http.post<IOrderResponse>(`${this._apiRoot}/orders`, this.order, {headers: headers, observe: 'response'})
+        .retry(3)
         .subscribe(res => {
           this.orderResponse = res.body;
+          this.orderResponse.TimePlaced = (new Date(Date.parse(this.orderResponse.TimePlaced))).toLocaleTimeString();
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.log('An error occurred:', err.error.message);
+          } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+          }
         });
 
       this.order = new Order;
